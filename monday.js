c3e1db63f2d8f8b0193e423ce53530ccc51588a6
ARSHAD HAMMED <125365781+Arshad3908@users.noreply.github.com>
2023-04-19 15:32:36 +0100

    Add files via upload

---
+++ monday.js
@@ -0,0 +1,18 @@
+let showAlert=function(){
+    let x=1;//type of x is number 
+    let y=3;//type of y is number
+    let name="kenny"
+    let age=54
+    let isold=age > 20;//type of isold is boolean
+    alert("hello,welcome" + age + name)
+}
+
+
+let showAnotherAlert=function(){
+    alert("whatsup buddy")
+}
+
+showAlert()
+showAnotherAlert()
+showAlert()
+showAnotherAlert()
